5142e999f8f417a45188b0d59ebace943c494dc8
hai <hai@LAPTOP-4DV9N6FJ>
2021-08-02 18:53:54 +0530

    4th commit
    Upload all files

---
+++ collections2/src/Arrayremove.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Arrayremove {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String>alist=new ArrayList<String>();
+		alist.add("raji");
+		alist.add("veni");
+		alist.add("vani");
+		System.out.println(alist);//displaying elements
+		alist.remove("veni");
+		System.out.println(alist);
+		alist.remove(1);
+		System.out.println(alist);
+		
+		
+	
+	}
+
+}
